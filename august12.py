b40ff2613740304872d2fbbf49d693ebe3da6f5d
rdinesh808 <rdinesh808@gmail.com>
2025-11-26 09:27:59 -0400

    script updated

--- august12.py
+++ august12.py
@@ -1,18 +1,45 @@
 import smtplib
-from datetime import datetime
+import calendar
+from datetime import date
+from prettytable import PrettyTable
 from email.message import EmailMessage
 
 APP_PASSWORD = "rgnd aasp wzyx fnhj"
 sender_email = "rdinesh808@gmail.com"
 receiver_email = "rdinesh808@gmail.com"
 
-today = datetime.strptime(datetime.now().strftime("%Y-%m-%d"), "%Y-%m-%d")
-august_12 = datetime.strptime("2026-08-12", "%Y-%m-%d")
-total_days = august_12 - today
-day = total_days.days
+# today = datetime.strptime(datetime.now().strftime("%Y-%m-%d"), "%Y-%m-%d")
+# august_12 = datetime.strptime("2026-08-12", "%Y-%m-%d")
+# total_days = august_12 - today
+# day = total_days.days
 
-subject = f"AUGUST - 12 - 2026 Total {day} days left"
-body = f"""Hi Dinesh,\n\nYou still need to wait {day} days to 12-August-2026.\n\nThanks"""
+table = PrettyTable()
+table.field_names = ["Year", "Month", "Count"]
+start_date = date.today()
+end_date = date(2026, 8, 12)
+total_days = (end_date - start_date).days
+month_counts = {}
+current_date = start_date
+while current_date <= end_date:
+    next_month_year = current_date.year + (1 if current_date.month == 12 else 0)
+    next_month = 1 if current_date.month == 12 else current_date.month + 1
+    first_day_next_month = date(next_month_year, next_month, 1)
+    if first_day_next_month <= end_date:
+        days_in_month = (first_day_next_month - current_date).days
+    else:
+        days_in_month = (end_date - current_date).days + 1
+    month_name = calendar.month_name[current_date.month] + " " + str(current_date.year)
+    month_counts[month_name] = days_in_month
+    current_date = first_day_next_month
+print(f"Total remaining days from {start_date} to {end_date}: {total_days}\n")
+for month, days in month_counts.items():
+    year = month.split()[1]
+    curr_month = month.split()[0]
+    count_days = days
+    table.add_row([year, curr_month, count_days])
+
+subject = f"12-August-2026 is coming in {total_days} days"
+body = f"""Hi Dinesh,\n\nTotal remaining days from {start_date} to {end_date}: {total_days}\n{table}\n\nThanks"""
 
 message = EmailMessage()
 message.set_content(body)
